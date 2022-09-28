052ec140735d2a82f26feba531895ad5c1947c7a
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-09-28 18:10:04 +0200

    nr checks

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:maand>", views.monthly_challenge_num),
+    path("<int:nr>", views.monthly_challenge_num),
     path("<str:maand>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,32 +1,38 @@
 from calendar import month
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
+monthly_challenges = {
+    "jan": "Eat no meat for an entire month!",
+    "feb": "Walk for at least 20 minutes every day!",
+    "mar": "Learn Django for at least 20 minutes every day!",
+    "apr": "Swim for 15 minutes in hot water!",
+    "may": "Practice mindfulness five times a week",
+    "jun": "Play with your dogs in the evening",
+    "jul": "Contemplate your achievments",
+    "aug": "Sing mantras with a group of people each day",
+    "sep": "Improve your poetry skills 1 hour a month",
+    "oct": "Learn Chinese 5 minutes a day",
+    "nov": "Do yoga at a gym for 45 minutes",
+    "dec": "Write a journal before going to sleep every day in between",
+}
 
 
 def index(req):
     return HttpResponse("<pre>Welcome to the challenges index page.</pre>")
 
 
-def monthly_challenge_num(req, maand):
-    return HttpResponse(maand)
+def monthly_challenge_num(req, nr):
+    m = list(monthly_challenges.keys())
+    if nr < len(m) and nr != 0:
+        redirect_month = m[nr-1]
+        return HttpResponseRedirect(f"/challenges/{redirect_month}")
+    else:
+        return HttpResponseNotFound("Invalid month man.")
 
 
 def monthly_challenge(request, maand):
-    monthly_challenges = {
-        "jan": "Eat no meat for an entire month!",
-        "feb": "Walk for at least 20 minutes every day!",
-        "mar": "Learn Django for at least 20 minutes every day!",
-        "apr": "Swim for 15 minutes in hot water!",
-        "may": "Practice mindfulness five times a week",
-        "jun": "Play with your dogs in the evening",
-        "jul": "Contemplate your achievments",
-        "aug": "Sing mantras with a group of people each day",
-        "sep": "Improve your poetry skills 1 hour a month",
-        "oct": "Learn Chinese 5 minutes a day",
-        "nov": "Do yoga at a gym for 45 minutes",
-        "dec": "Write a journal before going to sleep every day in between",
-    }
+
     try:
         return HttpResponse(monthly_challenges[maand])
     except:
