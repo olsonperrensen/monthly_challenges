31973aed6bc21af7b0d05c0e9aa7af6b9869830e
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-10-04 19:41:31 +0200

    fix bug line wrap %

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:nr>", views.monthly_challenge_num),
     path("<str:maand>", views.monthly_challenge, name="month-challenge")
 ]
